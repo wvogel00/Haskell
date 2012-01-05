bb18e04f168588c4636f10d9a5fa8e859b3b9b77
Wataru TOrii <eagle.hokkaido2317@gmail.com>
2012-01-05 17:22:56 +0900

    2012/01/05 17:22

--- Haskell/Calender/Menu.hs
+++ Haskell/Calender/Menu.hs
@@ -3,8 +3,18 @@ module Menu where
 import Graphics.UI.WX
 import Graphics.UI.WX.Menu
 
-myMenu = do file <- menuPane [text := "&File"]
-            mclose <- menuItem file [text := "$Close\tCtrl+C",
-                                     help := "close window"]
-            return [menuBar := [file]
-                   ,on (menu mclose) := return ()]
+myMenu = do
+ file <- menuPane [text := "&File"]
+ mClose <- menuItem file [text := "$Close\tCtrl+C",
+                          help := "close window"]
+
+ bHelp <- menuPane [text:="&Help"]
+ mVersion <- menuItem bHelp [text := "Version",
+                             help := "version info."]
+ mHelp    <- menuItem bHelp [text := "Help",
+                             help := "hot to use application"]
+
+ return [ menuBar := [file,bHelp]
+         ,on (menu mClose):= return ()
+         ,on (menu mHelp) := return ()
+         ,on (menu mVersion) := return ()]

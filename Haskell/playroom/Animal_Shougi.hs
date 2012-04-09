4a04ba2d07d74f79c99bced320db2dc6338f02d1
Wataru TOrii <eagle.hokkaido2317@gmail.com>
2012-04-09 17:54:34 +0900

    どうぶつしょうぎ

---
+++ Haskell/playroom/Animal_Shougi.hs
@@ -0,0 +1,59 @@
+module Animal_Shougi where
+
+import Data.IORef
+import Data.Vector
+
+data Animal = Lion | Elephant | Giraffe | Chick | Hen
+              deriving (Eq,Show)
+data Belong a = Mine a | Enemy a | Empty deriving Show
+
+type Pos = (Int,Int)
+type Board = [[Belong Animal]]
+
+type GameInfo = (Board , [Belong Animal] , [Belong Animal])
+
+height = 4
+width =  3
+
+point :: Animal -> Int
+point Chick    = 1
+point Elephant = 2
+point Giraffe  = 3
+point Hen      = 4
+point Lion     = 100  --勝利
+
+startBoard
+ = [
+    [   Mine Giraffe ,    Mine Lion ,  Mine Elephant ],
+    [          Empty ,   Mine Chick ,          Empty ],
+    [          Empty ,  Enemy Chick ,          Empty ],
+    [ Enemy Elephant ,   Enemy Lion ,  Enemy Giraffe ]
+   ]
+
+lion
+ = [(-1,-1) , (0,-1) , (1,-1),
+    (-1, 0) ,          (1, 0),
+    (-1, 1) , (0, 1) , (1, 1)
+   ]
+
+elephant
+ = [(-1,-1)     ,      (1,-1),
+
+    (-1, 1)     ,      (1, 1)
+   ]
+
+giraffe
+ = [          (0,-1)         ,
+    (-1,0)      ,      (1, 0),
+              (0, 1)
+   ]
+
+chick
+ = [          (0,-1)         ]        
+
+hen
+ = [(-1,-1) , (0,-1) , (1,-1),
+    (-1, 0) ,          (1, 0),
+              (0, 1)
+   ]
+

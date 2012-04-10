25f60ae7b7dd9942b8a7d7eb086e8ccc2f65ab9a
Wataru TOrii <eagle.hokkaido2317@gmail.com>
2012-04-10 22:53:36 +0900

    HTTP通信テスト

--- Haskell/test/HTTP_Test.hs
+++ Haskell/test/HTTP_Test.hs
@@ -1,12 +1,8 @@
---module HTTP_Test where
-
 import Network.HTTP (getRequest)
 import Network.HTTP.Base (rspBody)
 import Network.Browser(request,browse)
-
+import Control.Arrow
 main = do
  url <- getLine
  (_,res) <- browse.request.getRequest $ url
  putStr $ rspBody res
-
-

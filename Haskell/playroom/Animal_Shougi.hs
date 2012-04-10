2b722a3431623c5c4eb5aa777b58af5bda4a6acb
Wataru TOrii <eagle.hokkaido2317@gmail.com>
2012-04-10 11:33:07 +0900

    手番情報を追加

--- Haskell/playroom/Animal_Shougi.hs
+++ Haskell/playroom/Animal_Shougi.hs
@@ -4,23 +4,27 @@ import Data.IORef
 import Data.Vector
 
 data Animal = Lion | Elephant | Giraffe | Chick | Hen
-              deriving (Eq,Show)
+                                         deriving (Eq,Show)
 data Belong a = Mine a | Enemy a | Empty deriving Show
+data Turn = MyTurn | YourTurn            deriving Eq
 
 type Pos = (Int,Int)
 type Board = [[Belong Animal]]
 
-type GameInfo = (Board , [Belong Animal] , [Belong Animal])
+type GameInfo = (Board , Turn , [Belong Animal] , [Belong Animal])
 
 height = 4
 width =  3
+
+maximum  =  100
+_maximum = -100
 
 point :: Animal -> Int
 point Chick    = 1
 point Elephant = 2
 point Giraffe  = 3
 point Hen      = 4
-point Lion     = 100  --勝利
+point Lion     = 20  --勝利
 
 startBoard
  = [
@@ -56,4 +60,5 @@ hen
     (-1, 0) ,          (1, 0),
               (0, 1)
    ]
+
 

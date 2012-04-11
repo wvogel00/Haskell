1b3a02f66631d7e0dcf5c129795ba206b8e336c1
Wataru TOrii <eagle.hokkaido2317@gmail.com>
2012-04-11 21:56:00 +0900

    ソケット通信ゲーム：再帰前まで

--- Haskell/test/SocketClient.hs
+++ Haskell/test/SocketClient.hs
@@ -3,7 +3,34 @@ module SocketClient where
 import Network
 import System.IO 
 
-start = sendMessage "start"
+start = withSocketsDo $ do
+ hSetBuffering stdout NoBuffering
+ h <- connectTo "127.0.0.1" (PortNumber 8001)
+ hSetBuffering h LineBuffering
+
+ hPutStrLn h "gameStart"
+ statement <- hGetLine h
+ print statement
+ if statement == "start!!"
+  then game h
+  else return ()
+ hClose h
+
+game :: Handle -> IO()
+game h = do
+ seeCards h
+ putStrLn ">?"
+ getLine >>= hPutStrLn h
+
+ continuableOrNot h
+ where
+  seeCards h = hGetLine h >>= putStrLn
+  continuableOrNot h = do   --得点又はエラー文章を取得
+    str <- hGetLine h
+    putStrLn str
+    if head str == '*'
+      then hPutStrLn h ""
+      else game h
 
 chose number = withSocketsDo $ do
  hSetBuffering stdout NoBuffering
--- Haskell/test/SocketServer.hs
+++ Haskell/test/SocketServer.hs
@@ -3,14 +3,19 @@ import System.IO
 
 import System.Random
 import Control.Applicative
+import Control.Monad (when)
 import Data.IORef
+
 
 type Cards = IORef [Int]
 type Point = IORef Int
-type GameInfo = ((Point,Point) , (Cards,Cards))
+type GameInfo = (Point,Point , Cards,Cards)
+
+maxN :: Int
+maxN = 9
 
 card :: IO Int
-card = randomIO >>= return.(`mod` 10).abs
+card = randomIO >>= return.(+1).(`mod` maxN).abs
 
 takeCard 0 = return []
 takeCard n = do
@@ -20,56 +25,73 @@ takeCard n = do
 main :: IO ()
 main = withSocketsDo $ do
 
-    myCards'   <- takeCard 5
-    yourCards' <- takeCard 5
-    myCards    <- newIORef $ myCards'
-    yourCards  <- newIORef $ yourCards'
-    myPoint    <- newIORef 0
-    yourPoint  <- newIORef 0
-    let gameInfo = ((myPoint,yourPoint) , (myCards,yourCards))
+ myCards    <- newIORef =<< takeCard 5
+ yourCards  <- newIORef =<< takeCard 5
+ myPoint    <- newIORef 0
+ yourPoint  <- newIORef 0
+ let gameInfo = (myPoint,yourPoint,myCards,yourCards)
 
-    hSetBuffering stdout NoBuffering
-    (server gameInfo ) `catch` (const $ putStrLn "Exception caught.")
-    putStrLn "Connection closed."
+ hSetBuffering stdout NoBuffering
+ (server gameInfo ) `catch` (const $ putStrLn "Exception caught.")
+
+ putStrLn "Connection closed." -- 通信終了
 
 server :: GameInfo -> IO ()
 server info = do
     sock <- listenOn (PortNumber 8001)
-    repeats (receive sock info)
+    repeats (receive sock info) info
     sClose sock
 
-repeats :: Monad m => m Bool -> m () 
-repeats x =
-    x >>= (\x' -> if x' then (return ()) else repeats x)
+repeats :: IO Bool -> GameInfo -> IO () 
+repeats x info =
+    x >>= (\x' -> if x' then return () else server info)
 
 receive :: Socket -> GameInfo -> IO Bool
-receive sock info@((mP,yP),(mC,yC)) = do
+receive sock info = do
     (h,host,port) <- accept sock
     hSetBuffering h LineBuffering
-    yC' <- readIORef yC 
-    hPutStr h "chose numbers :-> " >> hPrint h yC'
 
     msg <- hGetLine h
-    if any (==read msg) yC'
-     then do
-           upDate (read msg) yC
-           k <- randomIO
-           upDate k mC
+    if msg == "gameStart"
+     then hPutStrLn h "start!!" >> game h info
+     else putStrLn ("See You!"++msg) >> return True
 
-           mC' <- readIORef mC
-           addPoint mP yP$ (read msg) - (mC' !! k `mod` length mC')
+game :: Handle -> GameInfo -> IO Bool
+game h info@(mP,yP,mC,yC) = do
+ mC' <- readIORef mC
+ yC' <- readIORef yC
 
-           showGameState h info
+ putStrLn $ "PC " ++ show mC'
+ putStrLn $ "PERSON " ++ show yC'
 
-           print ""
+ hPutStrLn h $ "chose number :-> "++ show yC'
 
-           putStrLn msg
-           hPutStrLn h $ reverse msg 
-           return $ null msg
-     else return False
+ input <- hGetLine h
+
+ if any (== read input) yC'
+   then do
+     rand <- randomIO
+     let myNumber = mC' !! (rand `mod` length mC')
+
+     putStrLn $ "PC chose " ++ show myNumber
+     putStrLn $ "PERSON chose" ++ input
+
+     upDate (read input) yC
+     upDate myNumber mC
+     addPoint mP yP (read input - myNumber)
+
+     showGameState h mP yP 
+   else
+     hPutStrLn h "*error! such number doesn't exist !"
+
+ yC'' <- readIORef yC
+ if null yC''
+  then game h info
+  else return True
+
 upDate n cards =
  readIORef cards >>=
- \cards' -> writeIORef cards $ filter (/= n) cards'
+  \cards' -> writeIORef cards $ filter (/= n) cards'
 
 addPoint mP yP sub
  | sub == 0 = return ()
@@ -79,10 +101,9 @@ addPoint mP yP sub
    addPoint' point n
     = readIORef point >>= \p -> writeIORef point (n+p)
 
-showGameState h ((mP,yP),(mC,yC)) = do
+showGameState h mP yP = do
  mP' <- readIORef mP
  yP' <- readIORef yP
- yC' <- readIORef yC
 
- hPutStrLn h $ "points of Player   :: " ++ show yP'
- hPutStrLn h $ "points of computer :: " ++ show mP' ++ "\n"
+ hPutStrLn h $ "# Player : computer = "
+                ++ show yP' ++ " : " ++ show mP'

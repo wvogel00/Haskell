d81f08fe102c728fdf3f6f619c6e346e47c2484c
Wataru TOrii <eagle.hokkaido2317@gmail.com>
2013-06-18 17:40:22 +0900

    実験用に修正

--- Haskell/playroom/DFT.hs
+++ Haskell/playroom/DFT.hs
@@ -1,42 +1,30 @@
-module DFT where
+module DFT2 where
 
-import Data.List
-import Control.Monad
 import Control.Applicative
 
-type ComplexNum = (Double,Double)
+fst' (v:_) = v
+snd' (_:v:_) = v
+thd (_:_:v:_) = v
+forth (_:_:_:v:_) = v
 
-dft :: Int -> [ComplexNum] -> [ComplexNum]
-dft n comps = map dft' [0..n-1] where
-	dft' i =
-		let d = 2.0*pi*fromIntegral i/fromIntegral n
-		in calc i d
-	calc i d = foldl sum' (0,0) $ map (\j -> (real j d,imag j d)) [0..n-1]
-	real j d = fst (comps !! j)*cos (d*fromIntegral j)
-	imag j d = negate $ snd (comps !! j)*sin (d*fromIntegral j)
-	sum' (x,ix) (v,iv) = (x+ix,v+iv)
-
-extractX,extractY,extractZ :: String -> ComplexNum
-extractX = (\x -> (read x,0)).head.words
-extractY = (\y -> (read y,0)).(\(_:y:_) -> y).words
-extractZ = (\z -> (read z,0)).(\(_:_:z:_) -> z).words
+dft :: Double -> [Double] -> Double -> Double
+dft n vs k = ps.foldl sum' (0,0) .map (uncurry f) $ zip [0,1..] vs where
+	f i v = (v*fst (w i),v*snd (w i))
+	w i= (cos (-2*pi*k*i/n) , sin (-2*pi*k*i/n))
+	sum' (a,b) (a2,b2) = (a+a2,b-b2)
+	ps (a,b) = a^2+b^2 -- power spect
 
 main = do
-	filename <- getLine
-	strs <- take 50.lines <$> readFile (filename++".txt")
-	let n =  200 -- ~1000ms 
-	let xDFT = map hz.zip [0..n-1].dft n $ add0 n (map extractX strs)
-	let yDFT = map hz.zip [0..n-1].dft n $ add0 n (map extractY strs)
-	let zDFT = map hz.zip [0..n-1].dft n $ add0 n (map extractZ strs)
-	writeFile (filename++"DFTX.txt").concat.map format $ xDFT
-	writeFile (filename++"DFTY.txt").concat.map format $ yDFT
-	writeFile (filename++"DFTZ.txt").concat.map format $ zDFT
-	mapM_ putStrLn (map show.take 10.reverse.sort $ xDFT)
-	mapM_ putStrLn (map show.take 10.reverse.sort $ yDFT)
-	mapM_ putStrLn (map show.take 10.reverse.sort $ zDFT)
+	file <- getLine
+	kxyz <- map (map read.words).lines <$> readFile file :: IO [[Double]]
+	let ks = map fst' kxyz
+	let n = fromIntegral $ length ks
+	let xs = map (dft n (map snd' kxyz)) ks
+	let ys = map (dft n (map thd kxyz)) ks
+	let zs = map (dft n (map forth kxyz)) ks
+	--mapM_ (putStrLn.format) $ zip ks $ zip3 xs ys zs
+	writeFile ("dft"++file) $ unlines.map format $ zip ks $ zip3 xs ys zs
+	putStrLn $ "done!\nresult is put in the file, dft" ++ file 
 
-max' a (i,v) = if a > v then a else v
-
-add0 n xs = xs ++ take (n-length xs) (repeat (0,0))
-format (hz,i) = show i ++ "\t" ++ show hz ++ "\n"
-hz (i,(v,iv)) = (v^2 + iv^2,i)
+format (k,(x,y,z)) =
+	show k ++ "\t" ++ show x ++ "\t" ++ show y ++ "\t" ++ show z
